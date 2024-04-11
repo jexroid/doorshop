71603a693961c8503674fb94a020eb50df6d51cf
Amirreza Farzan <amirfr2290@gmail.com>
2024-04-11 12:10:42 +0330

    init

---
+++ app/Filament/Resources/ProductionResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductionResource\Pages;
+use App\Filament\Resources\ProductionResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+
+class ProductionResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->required()
+                    ->label('نام کالا'),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->label('قیمت'),
+                Section::make()->schema([
+                    RichEditor::make('description')
+                        ->required()
+                        ->label('توضیحات')
+                        ->disableToolbarButtons([
+                            'codeBlock',
+                        ])
+                ]),
+                FileUpload::make('image')
+                ->image()
+                ->label('عکس محصول'),
+                Toggle::make('انتشار'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable()->searchable(),
+                TextColumn::make('title')->sortable()->searchable(),
+                TextColumn::make('description')->sortable()->searchable(),
+                ImageColumn::make('image'),
+                BooleanColumn::make('is_published')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProductions::route('/'),
+            'create' => Pages\CreateProduction::route('/create'),
+            'edit' => Pages\EditProduction::route('/{record}/edit'),
+        ];
+    }
+}
