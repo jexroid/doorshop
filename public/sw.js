667c210eb56c97c7e6b32c39ba32c876c4cc9bb4
Amirreza Farzan <amirfr2290@gmail.com>
2024-05-23 16:04:03 +0330

    sw.js updated

--- public/sw.js
+++ public/sw.js
@@ -1,43 +1,29 @@
-// const VERSION = 1.6;
-// const cacheName = 'static'
-// const cacheItems = [
-//     '/en',
-//     '/en/about_us',
-//     '/en/store',
-//     '/css/about.css',
-//     '/css/shop.css',
-//     '/css/store.css',
-//     '/css/index.css',
-//     '/css/app.css',
-//     '/js/app.js',
-//     '/js/index.js',
-//     '/js/shop.js',
-//     '/en/contact_us',
-//     '/logo.webp',
-//     '/click.ogg',
-//     '/generic.ogg',
+const VERSION = 1.7;
+const cacheName = 'static'
+const cacheItems = [
+    
 
-// ]
+]
 
-// this.addEventListener('install', event => {
-//     event.waitUntil(
-//         caches.open(cacheName).then(cache => {
-//             cache.addAll(cacheItems)
-//         })
-//     )
-// })
+this.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(cacheItems)
+        })
+    )
+})
 
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.ready.then(function (registration) {
-//         console.log('service worker updated', registration)
-//         registration.update();
-//     });
-// }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function (registration) {
+        console.log('service worker updated', registration)
+        registration.update();
+    });
+}
 
-// this.addEventListener('fetch', event => {
-//     event.respondWith(
-//         caches.match(event.request).then(response => {
-//             return response || fetch(event.request)
-//         })
-//     )
-// })
+this.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request)
+        })
+    )
+})
